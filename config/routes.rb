b9593e9a00e25155b04babe2acb526e806b73bc8
hiwa1223 <57945054+hiwa1223@users.noreply.github.com>
2019-12-04 20:37:40 +0900

    making_create_group_fanction

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to:"messages#index"
-  resources :users, only: [:edit, :update]
+  root to:"groups#index"
+  resources :users, only: [:index, :edit, :update]
+  resources :groups, only: [:update, :new, :create, :edit] do
+    resources :messages, only: [:index, :create]
+  end
 end
