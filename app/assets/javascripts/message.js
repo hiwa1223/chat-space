a77b71e55269e97c7e62cb5c9f818cdd76ecd1a8
hiwa1223 <57945054+hiwa1223@users.noreply.github.com>
2019-12-11 13:59:13 +0900

    1400 jidoukoushin

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,43 +1,43 @@
 $(function(){
   function buildHTML(data){
-    if ( data ) {
-    var html =`<div class="message">
-                <div class="upper-message">
-                <div class="upper-message__user-name">
-                  ${data.name}
-                </div>
-                <div class="upper-message__date">
-                  ${data.created_at}
-                </div>
-                </div>
-                <div class="lower-message">
-                <p class="lower-message__content">
-                  ${data.content}
-                </p>
-
-                </div>
-                </div>`
+    if ( data.image ) {
+      var html = `<div class="message">
+      <div class="upper-message">
+      <div class="upper-message__user-name">
+        ${data.name}
+      </div>
+      <div class="upper-message__date">
+        ${data.created_at}
+      </div>
+      </div>
+      <div class="lower-message">
+      <p class="lower-message__content">
+        ${data.content}
+      </p>
+      <img class="lower-message__image" src=${data.image}>
+      </div>
+      </div>`
     return html;
-  } else {
-    var html = `<div class="message">
-                <div class="upper-message">
-                <div class="upper-message__user-name">
-                  ${data.name}
-                </div>
-                <div class="upper-message__date">
-                  ${data.created_at}
-                </div>
-                </div>
-                <div class="lower-message">
-                <p class="lower-message__content">
-                  ${data.content}
-                </p>
-                <img class="lower-message__image" src="/uploads/message/image/9/%E3%82%82%E3%81%A3%E3%81%A8%E3%83%9B%E3%83%A1%E3%81%A6%E3%81%8F%E3%82%8C.jpg" alt="%e3%82%82%e3%81%a3%e3%81%a8%e3%83%9b%e3%83%a1%e3%81%a6%e3%81%8f%e3%82%8c">
-                </div>
-                </div>`
-      return html;
-    };
-  }
+  } else { 
+     var html =`<div class="message">
+      <div class="upper-message">
+      <div class="upper-message__user-name">
+        ${data.name}
+      </div>
+      <div class="upper-message__date">
+        ${data.created_at}
+      </div>
+      </div>
+      <div class="lower-message">
+      <p class="lower-message__content">
+        ${data.content}
+      </p>
+
+      </div>
+      </div>`
+    return html;
+  };
+}
 
   $("#new_message").on("submit", function(e){
     e.preventDefault() 
@@ -62,6 +62,7 @@ $(function(){
     })
     return false; 
   })
-
-  
 });
+
+// 自動更新
+// if(window.location.href.match(/\/groups\/\d+\/messages/))
