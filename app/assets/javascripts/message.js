c04b216fa214b1a82a4550ba90e08fb8df3d60ab
hiwa1223 <57945054+hiwa1223@users.noreply.github.com>
2019-12-11 14:47:20 +0900

    1450jidououshin

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -121,33 +121,26 @@ $(function(){
     return html;
   };
   var reloadMessages = function() {
-    //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
     last_message_id = $(".message").last().data("messageId");
     if(window.location.href.match(/\/groups\/\d+\/messages/))
     $.ajax({
-      //ルーティングで設定した通り/groups/id番号/api/messagesとなるよう文字列を書く
       url: `/groups/${httpId}/api/messages`,
-      //ルーティングで設定した通りhttpメソッドをgetに指定
       type: 'get',
       dataType: 'json',
-      //dataオプションでリクエストに値を含める
       data: {id: last_message_id}
     })
     .done(function(messages) {
         let html = buildHTML(data);
         $('.messages').append(html); 
         $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
-        //追加するHTMLの入れ物を作る
         var insertHTML = '';
-        //配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
         $.each(messages, function(i, message) {
           insertHTML += buildHTML(message)
       });
-      //メッセージが入ったHTMLに、入れ物ごと追加
       $('.messages').append(insertHTML);
     })
     .fail(function() {
-      console.log('error');
+      alert('自動更新に失敗しました');
     })
   }
   setInterval(reloadMessages, 7000);
