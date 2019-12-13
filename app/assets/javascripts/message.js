b41ebce4a48e22d2563f1fab66fd736ada6b4e3b
hiwa1223 <57945054+hiwa1223@users.noreply.github.com>
2019-12-13 21:59:42 +0900

    2159

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,67 +1,65 @@
 $(function(){
   function buildHTML(data){
-    if ( data ) {
-    var html =`<div class="message">
-                <div class="upper-message">
-                <div class="upper-message__user-name">
-                  ${data.name}
-                </div>
-                <div class="upper-message__date">
-                  ${data.created_at}
-                </div>
-                </div>
-                <div class="lower-message">
-                <p class="lower-message__content">
-                  ${data.content}
-                </p>
-
-                </div>
-                </div>`
+    if ( data.image ) {
+      var html = `<div class="message">
+      <div class="upper-message">
+      <div class="upper-message__user-name">
+        ${data.name}
+      </div>
+      <div class="upper-message__date">
+        ${data.created_at}
+      </div>
+      </div>
+      <div class="lower-message">
+      <p class="lower-message__content">
+        ${data.content}
+      </p>
+      <img class="lower-message__image" src=${data.image}>
+      </div>
+      </div>`
     return html;
-  } else {
-    var html = `<div class="message">
-                <div class="upper-message">
-                <div class="upper-message__user-name">
-                  ${data.name}
-                </div>
-                <div class="upper-message__date">
-                  ${data.created_at}
-                </div>
-                </div>
-                <div class="lower-message">
-                <p class="lower-message__content">
-                  ${data.content}
-                </p>
-                <img class="lower-message__image" src="/uploads/message/image/9/%E3%82%82%E3%81%A3%E3%81%A8%E3%83%9B%E3%83%A1%E3%81%A6%E3%81%8F%E3%82%8C.jpg" alt="%e3%82%82%e3%81%a3%e3%81%a8%e3%83%9b%e3%83%a1%e3%81%a6%e3%81%8f%e3%82%8c">
-                </div>
-                </div>`
+  } else { 
+     var html =`<div class="message">
+      <div class="upper-message">
+      <div class="upper-message__user-name">
+        ${data.name}
+      </div>
+      <div class="upper-message__date">
+        ${data.created_at}
+        </div>
+        </div>
+        <div class="lower-message">
+        <p class="lower-message__content">
+          ${data.content}
+        </p>
+        </div>
+        </div>`
       return html;
-    };
-  }
-
-  $("#new_message").on("submit", function(e){
-    e.preventDefault() 
-    let formdata = new FormData(this);
-    let url = $(this).attr("action");
-    $.ajax({
-      url: url,  //同期通信でいう『パス』
-      type: 'POST',  //同期通信でいう『HTTPメソッド』
-      data: formdata,  
-      dataType: 'json',
-      processData: false,
-      contentType: false,
-   })
-    .done(function(data) {
-      let html = buildHTML(data);
-      $('.messages').append(html); 
-      $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
-      $('form')[0].reset();
+      };
+    }
+    
+    $("#new_message").on("submit", function(e){
+      e.preventDefault() 
+      let formdata = new FormData(this);
+      let url = $(this).attr("action");
+      $.ajax({
+        url: url,  //同期通信でいう『パス』
+        type: 'POST',  //同期通信でいう『HTTPメソッド』
+        data: formdata,  
+        dataType: 'json',
+        processData: false,
+        contentType: false,
+     })
+      .done(function(data) {
+        console.log(data)
+        let html = buildHTML(data);
+        $('.messages').append(html); 
+        $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
+        $('form')[0].reset();
+      })
+      .fail(function(){
+        alert('error');
+      })
+      return false; 
     })
-    .fail(function(){
-      alert('error');
-    })
-    return false; 
-  })
-
-  
 });
